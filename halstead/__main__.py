3c1a5a2a3171934da06368564ff516fd31c2d38c
Robert Dougherty-Bliss <robert.w.bliss@gmail.com>
2018-09-08 19:37:52 -0400

    Add initial __init__ and __main__ files

---
+++ halstead/__main__.py
@@ -0,0 +1,45 @@
+from git import Repo
+from git.exc import GitCommandError
+from .process import get_dir_halstead
+import giturlparse
+import argparse
+
+
+def parse_args():
+    """TODO: Docstring for parse_args.
+    :returns: TODO
+
+    """
+    parser = argparse.ArgumentParser(prog="halstead", description="Analyze the Halstead complexity metrics of a git repository.")
+    parser.add_argument("repo", type=str, help="Valid path to git repo or user/project shorthand for GitHub repo")
+    parser.add_argument("clone_path", type=str, nargs="?", help="Valid path to git repo or user/project shorthand for GitHub repo")
+
+    return parser.parse_args()
+
+
+def main():
+    args = parse_args()
+    git_url = giturlparse.parse(args.repo)
+
+    if not git_url.valid:
+        raise ValueError("invalid git url")
+
+    if args.clone_path:
+        clone_path = args.clone_path
+    else:
+        clone_path = git_url.repo
+
+    try:
+        repo = Repo.clone_from(git_url.urls["https"], clone_path)
+    except GitCommandError as e:
+        if e.status == 128:
+            raise IOError("clone path `{}` is non-empty".format(clone_path))
+
+        raise e
+
+    results = get_dir_halstead(clone_path)
+    print(results)
+
+
+if __name__ == "__main__":
+    main()
