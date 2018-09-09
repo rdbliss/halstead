6251379bf43baec84143f5bba8c26c72523d6d2d
Robert Dougherty-Bliss <robert.w.bliss@gmail.com>
2018-09-08 21:55:00 -0400

    Create function for pulling repos

--- halstead/__main__.py
+++ halstead/__main__.py
@@ -1,8 +1,34 @@
-from .process import get_dir_halstead
-from .output import plot_function_length_pairs
 DESCRIPTION = "Analyze the Halstead complexity metrics of a git repository."
 REPO_HELP = "Valid path to git repo or user/project shorthand for GitHub repo."
 CLONE_PATH_HELP = "Directory for cloned repo."
+
+
+def pull_repo(git_url, clone_path):
+    import git
+    try:
+        repo = git.Repo.clone_from(git_url.urls["https"], clone_path)
+    except git.exc.GitCommandError as e:
+        if e.status != 128:
+            raise e
+
+        # The directory already exists.
+        # Is it a git repository?
+        try:
+            repo = git.Repo(clone_path)
+        except git.InvalidGitRepositoryError:
+            raise IOError("clone-path `{}`".format(clone_path) +
+                       " is non-empty and not a git repository")
+
+        # Is the repo tracking the same thing that we are?
+        for remote in repo.remotes:
+            for url in git_url.urls.values():
+                if url in remote.urls:
+                    # Yes, so pull it.
+                    remote.pull()
+                    return
+
+        raise IOError("clone-path `{}`".format(clone_path) +
+                      "is a git repository distinct from the given url")
 
 
 def parse_args():
@@ -12,7 +38,6 @@ def parse_args():
     """
     import argparse
     import giturlparse
-    import git
 
     parser = argparse.ArgumentParser(prog="halstead", description=DESCRIPTION)
     parser.add_argument("repo", type=str, help=REPO_HELP)
